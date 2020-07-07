986c51aa1564b6ff59ed4de91a629c01181233f3
sebastian velasquez <zoedevelop@gmail.com>
2020-07-06 21:28:59 -0500

    segunda entrega del curso de angular

--- app.js
+++ app.js
@@ -1,51 +1,52 @@
-(
-    function () {
-        'use strict';
+(function () {
+    'use strict'
 
-        let lista1 =[{
-            cantidad: 5,
-            nombre: "jugos"
-        },
-        {
-            cantidad: 2,
-            nombre: "manzanas"
-        },
-        {
-            cantidad: 15,
-            nombre: "cervezas"
-        },
-        {
-            cantidad: 20,
-            nombre: "galletas"
-        },
-        {
-            cantidad: 200,
-            nombre: "cigarillos"
-        }
-] 
+    var shoppingListModule = angular.module('market', []);
 
-        angular.module('marketApp',[])
-        .controller('listComprar',listComprar)
-        .controller('listComprado', listComprado);
+    shoppingListModule.controller('compras', compras)
+    shoppingListModule.controller('carga', carga)
+    shoppingListModule.service('comprado', comprado)
 
+    function comprado() {
+        var comprado = this;
 
-        listComprar.$inject = ['$scope']
+        comprado.compras = [
+            { name: 'beer', quantity: 12 },
+            { name: 'cookies', quantity: 40 },
+            { name: 'Oranges', quantity: 1 },
+            { name: 'meat', quantity: 5 },
+            { name: 'milk', quantity: 2 },
+        ];
 
-        function listComprar($scope){
-            $scope.comprar = lista1;
-            $scope.msj = function () {
-                if($scope.comprar.length == 0){
-                    $scope.have = "Todo Esta Comprado";
-                }else{
-                    $scope.have = "Continue Comprando";
-                }  
-                return have;   
-            };
-          
-        }
-        function listComprado(){
-            
+        comprado.boughtList = [];
+
+        comprado.getcompras = function () {
+            return comprado.compras;
         }
 
+        comprado.getBoughtList = function () {
+            return comprado.boughtList;
+        }
+
+        comprado.markAsBought = function (index) {
+            comprado.boughtList.push(comprado.compras[index]);
+            comprado.compras.splice(index, 1);
+        }
     }
-)()
+
+    compras.$inject = ['comprado'];
+    function compras(comprado) {
+        var showcompras = this;
+        showcompras.items = comprado.getcompras();
+
+        showcompras.markAsBought = function (itemIndex) {
+            comprado.markAsBought(itemIndex);
+        };
+    }
+
+    carga.$inject = ['comprado'];
+    function carga(comprado) {
+        var alreadyBoughtList = this;
+        alreadyBoughtList.items = comprado.getBoughtList();
+    }
+})();
